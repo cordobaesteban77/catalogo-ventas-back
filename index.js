57b40b6cc91d08bc158ca7ddd2cfc7ac8a97524e
Esteban <cordobaesteban77@gmail.com>
2025-09-02 06:11:16 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+//middlewares
+app.use(express.json())
+
+//rutas
+
+
+app.listen(3001, () => {
+    console.log("Server runing in port 3001");
+})
