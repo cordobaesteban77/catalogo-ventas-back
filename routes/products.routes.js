b273a3ab8b1bbfeaf6ca4eb939bf374fc7eb8a79
Esteban <cordobaesteban77@gmail.com>
2025-09-08 21:06:32 -0300

    agrego el cambiar estado del producto para habilitar o deshabilitar

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const { getAllProducts, getProductById, createProduct, editProduct, deleteProduct } = require("../controllers/products.controllers")
+const { getAllProducts, getProductById, createProduct, editProduct, deleteProduct, changeStateProduct } = require("../controllers/products.controllers")
 const router = express.Router()
 
 router.get("/", getAllProducts)
 router.get("/:id", getProductById)
 router.post("/", createProduct)
 router.put("/:id", editProduct)
+router.put("/changeState/:idProduct" , changeStateProduct)
 router.delete("/:id", deleteProduct)
 
 module.exports = router
