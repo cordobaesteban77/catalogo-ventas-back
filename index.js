4abad4e00194366f694b077d276311daa9d07cca
Córdoba Esteban Augusto <cordobaesteban77@gmail.com>
2025-09-29 05:27:30 -0300

    configuro cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("./db/db.config")
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const path = require("path")
 const morgan = require("morgan")
 
@@ -8,6 +9,16 @@ const morgan = require("morgan")
 app.use(express.json())
 app.use("/public", express.static(path.join(__dirname, "public")))
 app.use(morgan("dev"))
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 //rutas
 app.use("/products", require("./routes/products.routes"))
